005f315f6c5436a312e02eff5679c7fff0022964
HoJun Yang <didghwns0514@gmail.com>
2021-08-14 16:29:19 +0900

    doc,test(.md/.java): Add files regarding lecture cp5-3
    Content
    - Java Bean access methods; Name, Interface.class, ImplementedClass.class
    - Why use Interface and use both names and interface for bean find

---
+++ 프로젝트(Top Folder)/Code-Folder-FirstSpringBoot/core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,67 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac;
+
+    @BeforeEach
+    public void beforeEach(){
+        ac  = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름 없이 타입으로만 조회")
+    void findBeanByType(){
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회 - 구현에 의존하는 것이므로, 좋은 코드는 아니지만... 가능")
+    void findBeanByName2(){
+
+        //Service Impliments는 구현체
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findByNameX() {
+
+        // 반드시 해당 exception이 뜬다는 설정
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("XXXX", MemberService.class));
+    }
+}
